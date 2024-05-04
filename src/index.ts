6dfc33d72207b44a61b22810571c8853898eb768
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2024-05-04 12:15:14 -0400

    API Working

---
+++ src/index.ts
@@ -0,0 +1,66 @@
+/*
+ * CentsBot API
+ */
+import { serve } from "@hono/node-server";
+import { config } from "dotenv";
+import { Hono } from "hono";
+import {
+  submitQuestionDocuments,
+  submitQuestionGeneralGPT
+} from "./libraries/azureHelpers.js";
+import {
+  loginDrupal,
+  logoutDrupal,
+  post2Drupal
+} from "./libraries/drupalHelpers.js";
+
+// Load environment variables
+config({ path: "/etc/gptbot/.env" });
+
+// Constants
+const drupalUrl = process.env.DRUPAL_BASE_URL;
+const azBaseUrl = process.env.AZ_BASE_URL;
+const azApiKey = process.env.AZ_API_KEY;
+const azSearchUrl = process.env.AZ_SEARCH_URL;
+const azSearchKey = process.env.AZ_SEARCH_KEY;
+const azIndexName = process.env.AZ_INDEX_NAME;
+const azPMIndexName = process.env.AZ_PM_INDEX_NAME;
+const uname = process.env.DRUPAL_USERNAME;
+const pword = process.env.DRUPAL_PASSWORD;
+
+const app = new Hono();
+
+const loginValues = await loginDrupal(drupalUrl, uname, pword);
+console.log(loginValues);
+
+// Routes
+app.get("/", (c) => {
+  return c.render(`
+    <h1>CentsBot API</h1>
+    <h2></h2>
+    <p>This API is used for CentsBot question Submissions and Answers.</p>
+  `);
+});
+
+app.get("/hello/:name", (c) => {
+  return c.render(`
+    <h1>Hello ${c.req.param("name")}</h1>
+    Hope you have a good day!
+  `);
+});
+
+app.post("/api/submit/:node", (c) => {
+  return c.text(`{
+    status: "success",
+    id: ${c.req.param("node")},
+    message: "${c.req.param("node")} submitted successfully"
+  }`);
+});
+
+const port = 3000;
+console.log(`Server is running on port http://localhost:${port}`);
+
+serve({
+  fetch: app.fetch,
+  port
+});
